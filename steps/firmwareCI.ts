c73035bad0bee605ed382c733af3690ede73dfe3
Markus Tacker <Markus.Tacker@NordicSemi.no>
2021-06-16 12:23:18 +0200

    fix: use correct matcher

--- steps/firmwareCI.ts
+++ steps/firmwareCI.ts
@@ -1,34 +1,30 @@
 import {
-	regexGroupMatcher,
 	StepRunnerFunc,
 	InterpolatedStep,
+	regexMatcher,
 } from '@nordicsemiconductor/e2e-bdd-test-runner'
 
 export const firmwareCIStepRunners = (): ((
 	step: InterpolatedStep,
-) => StepRunnerFunc<any> | false)[] => [
-	regexGroupMatcher<any>(/^the Firmware CI run device log should contain$/)(
-		async (_, step, runner) => {
-			if (step.interpolatedArgument === undefined) {
-				throw new Error('Must provide argument!')
-			}
-			const expected = step.interpolatedArgument
-				.split('\n')
-				.map((s) => s.trim())
+) => StepRunnerFunc<{ deviceLog: string[] }> | false)[] => [
+	regexMatcher<{ deviceLog: string[] }>(
+		/^the Firmware CI run device log should contain$/,
+	)(async (_, step, runner) => {
+		if (step.interpolatedArgument === undefined) {
+			throw new Error('Must provide argument!')
+		}
+		const expected = step.interpolatedArgument.split('\n').map((s) => s.trim())
 
-			const matches = expected.map((e) => ({
-				matches: (runner.store.deviceLog as string[]).filter((s) =>
-					s.includes(e),
-				),
-				expected: e,
-			}))
+		const matches = expected.map((e) => ({
+			matches: runner.world.deviceLog.filter((s) => s.includes(e)),
+			expected: e,
+		}))
 
-			matches.map(({ matches, expected }) => {
-				if (matches.length === 0)
-					throw new Error(`deviceLog did not contain "${expected}"`)
-			})
+		matches.map(({ matches, expected }) => {
+			if (matches.length === 0)
+				throw new Error(`deviceLog did not contain "${expected}"`)
+		})
 
-			return matches.map(({ matches }) => matches).flat()
-		},
-	),
+		return matches.map(({ matches }) => matches).flat()
+	}),
 ]
